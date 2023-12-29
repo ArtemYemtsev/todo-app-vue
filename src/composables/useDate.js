ea29852fc8b4a4d1bdd4d714ace44b117d62f404
ArtemYemtsev <artem.y.solardigital@gmail.com>
2023-12-29 13:46:09 +0200

    Added font, fix date (month)

--- src/composables/useDate.js
+++ src/composables/useDate.js
@@ -3,7 +3,7 @@ export default function useDate() {
   
   const currentDate = {
     date: DATE.getDate() < 10 ? `0${DATE.getDate()}` : DATE.getDate(),
-    month: DATE.getMonth() < 10 ? `0${DATE.getMonth()}` : DATE.getMonth(),
+    month: DATE.getMonth() < 10 ? `0${DATE.getMonth() + 1}` : DATE.getMonth() + 1,
     year: DATE.getFullYear(),
   };
 
